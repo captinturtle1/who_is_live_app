8c9db176872462ec7bb42e10fc49e1dd9b5d9b5e
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-08-26 16:05:40 -0600

    Buttons
    Adde buttons to bottom
    
    Made AddRemove page

--- components/AddRemove.tsx
+++ components/AddRemove.tsx
@@ -4,53 +4,34 @@ import { SafeAreaView, Text, View, Image, ScrollView, TextInput, Button } from '
 import { styled } from "nativewind";
 import { useState, useEffect } from 'react';
 
-import AddRemove from './components/AddRemove';
-
 const SView = styled(View);
 const SText = styled(Text);
 const SImage = styled(Image);
 const SScrollView = styled(ScrollView);
 const STextInput = styled(TextInput);
 
-function App(): JSX.Element {
-  const [isAddRemoveOpen, setIsAddRemoveOpen] = useState(false);
-  return (
-    <SafeAreaView>
-      {isAddRemoveOpen ? 
-        <AddRemove setIsAddRemoveOpen={setIsAddRemoveOpen}/> 
-      :
+export default function AddRemove({setIsAddRemoveOpen}) {
+    return(
         <SView className="flex h-full bg-slate-800 p-2">
           <SView className='flex-grow'/>
           <SView className='flex flex-row gap-2'>
             <SView className='flex-1'>
               <Button
-                onPress={() => setIsAddRemoveOpen(true)}
-                title='Edit'
+                onPress={() => setIsAddRemoveOpen(false)}
+                title='Cancel'
                 color='#3b82f6'
                 accessibilityLabel='Open edit panel button'
               />
             </SView>
             <SView className='flex-1'>
               <Button
-                onPress={() => setIsAddRemoveOpen(true)}
-                title='Refresh'
+                onPress={() => setIsAddRemoveOpen(false)}
+                title='Save'
                 color='#3b82f6'
                 accessibilityLabel='Refresh list button'
               />
             </SView>
-            <SView className='flex-1'>
-              <Button
-                onPress={() => setIsAddRemoveOpen(true)}
-                title='Help'
-                color='#3b82f6'
-                accessibilityLabel='View help'
-              />
-            </SView>
           </SView>
         </SView>
-      }
-    </SafeAreaView>
-  );
+    )
 }
-
-export default App;
